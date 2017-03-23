e0ce72405a94d659429cba9a49e2fe087c0c8879
Raghvendra Sahu <rx.sahu@gmail.com>
2017-03-24 02:14:23 +0530

    initial commit

---
+++ src/main/java/com/raghavx/client/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.raghavx.client;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SmppClientPanelApplication.class);
+	}
+
+}
---
+++ src/main/java/com/raghavx/client/SmppClientPanelApplication.java
@@ -0,0 +1,12 @@
+package com.raghavx.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmppClientPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmppClientPanelApplication.class, args);
+	}
+}
