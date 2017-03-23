9209b2c2447b7c42d2c1c0cc58a2cd0a597402e0
Raghvendra Sahu <rx.sahu@gmail.com>
2017-03-24 02:41:51 +0530

    More async

--- src/main/java/com/raghavx/client/controllers/SendSmsController.java
+++ src/main/java/com/raghavx/client/controllers/SendSmsController.java
@@ -1,16 +1,33 @@
 package com.raghavx.client.controllers;
 
+import java.util.concurrent.Callable;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.raghavx.client.services.SendSmsService;
 
+/**
+ * 
+ * @author raghav
+ *
+ */
 @RestController
 public class SendSmsController {
 
 	@Autowired
 	private SendSmsService sendSmsService;
-	public void send(){
-		sendSmsService.send();
+
+	public Callable<String> send() {
+		Callable<String> callable = new Callable<String>() {
+
+			@Override
+			public String call() throws Exception {
+				// TODO Auto-generated method stub
+				sendSmsService.send();
+				return null;
+			}
+		};
+		return callable;
 	}
 }
